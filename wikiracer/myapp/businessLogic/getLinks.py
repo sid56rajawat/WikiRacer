456e1467bf15bff59fe87ef3f0360b450103b0df
Shivam Rajawat <90323915+sid56rajawat@users.noreply.github.com>
2023-03-12 21:55:14 +0530

    Heuristic Search in progress

--- wikiracer/myapp/businessLogic/getLinks.py
+++ wikiracer/myapp/businessLogic/getLinks.py
@@ -2,7 +2,8 @@ import httplib2
 import time
 from bs4 import BeautifulSoup, SoupStrainer
 
-def get_links(url): # fetches all the valid links on a given url
+def get_links(query): # fetches all the valid links on a given url
+    url = "https://en.wikipedia.org/wiki/" + query
     http = httplib2.Http()
     response, content = http.request(url)
 
@@ -11,13 +12,14 @@ def get_links(url): # fetches all the valid links on a given url
         linkString = link['href']
         if(linkString.startswith("/wiki/") and ":" not in linkString and "#" not in linkString):
             links.add(linkString[6:])
-
+        # if(len(links) > 500): # branching factor
+        #     break
     return links
 
 if __name__ == '__main__':
     thing = input("Search Wiki : ")
     startTime = time.time()
-    links = get_links("https://en.wikipedia.org/wiki/" + thing)
+    links = get_links(thing)
     [print(i) for i in links]
     print("Total links on",thing,"=",len(links))
     print("Time taken =",time.time()-startTime,"seconds")
