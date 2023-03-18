dbd3cce215578268476fef66db0ba69c36e7ca25
Shivam Rajawat <90323915+sid56rajawat@users.noreply.github.com>
2023-03-18 10:48:55 +0530

    Successfully Dockerized the project

--- wikiracer/wikiracer/settings.py
+++ wikiracer/wikiracer/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'wikiracer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'D:\Documents\CODE\Bootcamp\wikiracer\myapp\templates'],
+        'DIRS': [r'../myapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,7 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# TEMPLATE_DIRS = (
+#     os.path.join(BASE_DIR, "myapp/templates/"),
+# )
